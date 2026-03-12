e6c9c377de05c021004274bcf1f25f8b9e9d07fb
Quadnicon <barnabe.joao@escola.pr.gov.br>
2026-03-12 09:05:46 -0300

    Add files via upload

---
+++ java/brake_continue.js
@@ -0,0 +1,14 @@
+let carros_estacionados=0;
+const limitemaximo=7;
+    console.log("---Iniciando verificação de vagas---")
+for(let vaga=1;vaga<=10;vaga++){
+    if(vaga===4||vaga===7||vaga===3){
+        console.log(`vaga ${vaga}: [interditada]-Pulando...`);
+        continue;}
+    carros_estacionados++;
+    console.log(`vaga ${vaga}: [livre]->Carro estacionado"(total:${carros_estacionados})`);
+    if(carros_estacionados===limitemaximo){
+        console.log("---Limite atingido!fechar portão---");
+        break;}
+    }
+    console.log("relatorio final: Operação de entrada encerrada");

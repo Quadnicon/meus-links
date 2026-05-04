b9e9d886b2b7f81a42789f263cefeb091e123006
Quadnicon <barnabe.joao@escola.pr.gov.br>
2026-05-04 09:38:13 -0300

    Delete java directory

--- java/objeto.js
+++
@@ -1,6 +0,0 @@
-let pessoa={nome:"Maria",idade:"30"};
-console.log(pessoa.nome);
-console.log(pessoa["idade"]);
-pessoa.profissao="desenvolvedora";
-pessoa.idade=31;
-console.log(pessoa);
--- java/teste.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world!");
-console.log(123456789);
-console.log("João");
-x=8+9;
-console.log(x);
-y=81;
-z=9;
-T=y+z;
-console.log("O resultado é:",T);
